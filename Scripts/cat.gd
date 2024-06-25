89536428832e007df107ef66401eaaed034c3b82
Omar Rojas Rubino <orojas@heuristyc.com>
2024-06-25 15:49:04 -0300

    adding cats animations and cat movement [not done]

---
+++ Scripts/cat.gd
@@ -0,0 +1,65 @@
+class_name Cat extends CharacterBody2D
+
+@export var animation_tree: AnimationTree
+@export var body_area: Area2D
+@export var sprite: Sprite2D
+
+const SPEED = 200.0
+
+#Input actions
+const LEFT = "left"
+const RIGHT = "right"
+const UP = "up"
+const DOWN = "down"
+
+const INVENCIBILITY_TIME = 1
+
+enum {
+	WALK,
+	PURR,
+}
+
+var state
+var input_direction = Vector2.ZERO
+
+func _ready():
+	input_direction = Vector2(0, 1)
+	update_blend_directions()
+	
+func _physics_process(delta):
+	input_direction = Input.get_vector(LEFT, RIGHT, UP, DOWN)
+	if input_direction != Vector2.ZERO:
+		state = WALK
+	match state:
+		WALK:
+			move_state(delta)
+		PURR:
+			purr_state()
+		_: 
+			pass
+
+func move_state(delta):
+	if input_direction == Vector2.ZERO:
+		set_animation_conditions("parameters/conditions/idle")
+	else:
+		set_animation_conditions("parameters/conditions/is_walking")
+		update_blend_directions()
+			
+	if Input.is_action_just_pressed("purr"):
+		state = PURR
+		
+	velocity = input_direction * SPEED
+	move_and_slide()
+	
+func set_animation_conditions(condition):
+	animation_tree["parameters/conditions/idle"] = false
+	animation_tree["parameters/conditions/is_walking"] = false
+	animation_tree["parameters/conditions/is_purring"] = false
+	animation_tree[condition] = true
+
+func update_blend_directions():
+	#animation_tree["parameters/idle/blend_position"] = input_direction
+	animation_tree["parameters/walk/blend_position"] = input_direction
+
+func purr_state():
+	set_animation_conditions("parameters/conditions/is_purring")
