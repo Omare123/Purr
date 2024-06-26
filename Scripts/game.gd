c1544bc27110cae652e45e55851d49fa4d9ce6c9
Omar Rojas Rubino <orojas@heuristyc.com>
2024-06-26 17:52:28 -0300

    random position generation

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,12 +1,15 @@
 extends Node2D
 const NPC_SCENE = preload("res://Scenes/Characters/npc.tscn")
 @onready var cat = $Cat
+@onready var tile_map = $TileMap
+
 const sprites_path = "res://Assets/Sprites/"
 const NPC_NAMES = "res://Resources/npc_sprite_names.json"
 const npc_max_nbr = 7
 const npc_to_add = 10
 
 func _ready():
+	
 	var json_as_text = FileAccess.get_file_as_string(NPC_NAMES)
 	var json_as_dict = JSON.parse_string(json_as_text)["data"]
 	for n in npc_to_add:
@@ -19,7 +22,9 @@ func _ready():
 		print(sprites_path + json_as_dict[i])
 		img.load(sprites_path + json_as_dict[i])
 		npc_resource.texture = tex.create_from_image(img)
+		var x_position = randi_range(0, 1150)
+		var y_position = randi_range(650, 0)
 		npc.npc_resource = npc_resource
 		npc.player = cat
-		npc.position = Vector2(i, 0)
+		npc.position = Vector2(x_position, y_position)
 		add_child(npc)
