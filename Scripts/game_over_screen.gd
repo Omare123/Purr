f9c957c5a227bccc39b5d576f285e62f8d966e19
Omar Rojas Rubino <orojas@heuristyc.com>
2024-06-29 10:17:27 -0300

    adding instructions to the main menu and the pause manu

--- Scripts/game_over_screen.gd
+++ Scripts/game_over_screen.gd
@@ -1,5 +1,8 @@
 extends CanvasLayer
 @export var timer: Label
+@onready var pause_ui = $Control
+@onready var back = $Back
+@onready var instructions = $Instructions
 
 func _ready():
 	get_tree().paused = true
@@ -18,3 +21,14 @@ func _on_quit_button_down():
 func _on_resume_button_down():
 	get_tree().paused = false
 	queue_free()
+
+
+func _on_back_button_down():
+	pause_ui.visible = true
+	back.visible = false
+	instructions.visible = false
+
+func _on_instructions_button_down():
+	pause_ui.visible = false
+	back.visible = true
+	instructions.visible = true
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,9 +2,22 @@ class_name MainMenu extends Control
 @onready var start_button = $MarginContainer/VBoxContainer/Start_Button
 @onready var quit_button = $MarginContainer/VBoxContainer/Quit_Button
 @onready var main_level = preload("res://Scenes/game.tscn")
+@onready var controls = $Controls
+@onready var main = $Main
+@onready var back_button = $Back
 
 func _on_start_button_button_down():
 	get_tree().change_scene_to_packed(main_level)
 
 func _on_quit_button_button_down():
 	get_tree().quit()
+
+func _on_controls_button_down():
+	main.visible = false
+	controls.visible = true
+	back_button.visible = true
+
+func _on_back_button_down():
+	main.visible = true
+	controls.visible = false
+	back_button.visible = false
